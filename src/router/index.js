f01ca01c6241ec2467890027d2ba7d45acee49aa
kurtneil26 <kcnacua@usm.edu.ph>
2025-08-16 09:21:42 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+// filepath: c:\Users\User\OneDrive\Desktop\VUE.JS\src\router\index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../views/admin/dashboard.vue'
+import Pending from '../views/admin/pending.vue'
+import Approved from '../views/admin/approved.vue'
+import Rejected from '../views/admin/rejected.vue'
+import Account from '@/views/admin/account.vue'
+import History from '@/views/admin/history.vue'
+import ApplicantBase from '../views/applicant/ApplicantBase.vue'
+import ApplicantDashboard from '../views/applicant/Dashboard.vue'
+import ApplicantApply from '../views/applicant/Apply.vue'
+
+const routes = [
+  { path: '/', name: 'Dashboard', component: Dashboard },
+  { path: '/admin/pending', name: 'Pending', component: Pending },
+  { path: '/admin/approved', name: 'Approved', component: Approved },
+  { path: '/admin/rejected', name: 'Rejected', component: Rejected },
+  { path: '/admin/account', name: 'Account', component: Account },
+  { path: '/admin/history', name: 'History', component: History },
+  {
+    path: '/applicant',
+    component: ApplicantBase,
+    children: [
+      { path: 'dashboard', component: ApplicantDashboard },
+      { path: 'apply', component: ApplicantApply },
+    ]
+  },
+  // Add other routes here
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
